4e394cd54b2bfdae13b7fd5e18e32729b24c64dd
albertoomarcos <albertoo.marcos@gmail.com>
2019-02-22 17:08:10 +0100

    Creacion Proyecto

---
+++ app/src/main/java/com/example/portatil/cdi_empresaseguridad/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.portatil.cdi_empresaseguridad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
