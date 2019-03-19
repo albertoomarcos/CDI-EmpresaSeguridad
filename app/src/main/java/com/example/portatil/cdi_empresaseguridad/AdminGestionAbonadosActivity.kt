ae3ac120eba335f4bfa4fe461d6b8793f17692b8
albertoomarcos <albertoo.marcos@gmail.com>
2019-03-19 17:47:18 +0100

    Añadiendo dos layout mas

---
+++ app/src/main/java/com/example/portatil/cdi_empresaseguridad/AdminGestionAbonadosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.portatil.cdi_empresaseguridad
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AdminGestionAbonadosActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_gestionabonados)
+    }
+}
