25018a731d9d838ee3f3977ab97c1799119e2943
Javier Gines <javiginessanchez@hotmail.com>
2019-04-04 21:25:49 +0200

    Solve #3

--- app/src/main/java/com/example/portatil/cdi_empresaseguridad/DomoticaFinal.kt
+++ app/src/main/java/com/example/portatil/cdi_empresaseguridad/DomoticaFinal.kt
@@ -3,26 +3,52 @@ package com.example.portatil.cdi_empresaseguridad
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 
 class DomoticaFinal: AppCompatActivity() {
+
+    private lateinit var name: EditText
+    private lateinit var descripcion: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_domotica_fin)
 
-        var imAtras = findViewById(R.id.imAtras) as ImageView
-        var imAniadir = findViewById(R.id.imAniadir) as ImageView
+        val imAtras = findViewById(R.id.imAtras) as ImageView
+        val imAniadir = findViewById(R.id.imAniadir) as ImageView
 
         imAtras.setOnClickListener {
             // your code to perform when the user clicks on the button
             Toast.makeText(this@DomoticaFinal, "Volver atras", Toast.LENGTH_SHORT).show()
 
         }
+
+        name = findViewById(R.id.editNombre)
+        name.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                imAniadir.isEnabled = (!s.isNullOrBlank() && !descripcion.text.isNullOrBlank())
+            }
+        })
+
+        descripcion = findViewById(R.id.editDescripcion)
+        descripcion.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                imAniadir.isEnabled = (!s.isNullOrBlank() && !descripcion.text.isNullOrBlank())
+            }
+        })
+        imAniadir.isEnabled = false
         imAniadir.setOnClickListener {
-            // your code to perform when the user clicks on the button.
-            //HABRIA QUE REVISAR QUE PONGA ALGO EN LOS EDIT TEXT!!! NO SE HACERLO
             Toast.makeText(this@DomoticaFinal, "Añadido con exito...", Toast.LENGTH_LONG).show()
             val mIntent = Intent(this, DomoticaNuevaPlanta::class.java)
             startActivity(mIntent)
