bf63d0f1ff55d5da5b477c04ffbfbd056d31cbf0
Alberto Marcos Blasco <38374836+albertoomarcos@users.noreply.github.com>
2019-04-20 19:05:35 +0200

    SalaComunOtros

--- app/src/main/java/com/example/portatil/cdi_empresaseguridad/DomoticaSalaComun.kt
+++ app/src/main/java/com/example/portatil/cdi_empresaseguridad/DomoticaSalaComun.kt
@@ -42,7 +42,8 @@ class DomoticaSalaComun: AppCompatActivity() {
         }
         imOtros.setOnClickListener{
             // your code to perform when the user clicks on the button
-            Toast.makeText(this@DomoticaSalaComun, "5", Toast.LENGTH_SHORT).show()
+            val mIntent = Intent(this, DomoticaSalaComunOtros::class.java)
+            startActivity(mIntent)
 
         }
     }
---
+++ app/src/main/java/com/example/portatil/cdi_empresaseguridad/DomoticaSalaComunOtros.kt
@@ -0,0 +1,37 @@
+package com.example.portatil.cdi_empresaseguridad
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+
+class DomoticaSalaComunOtros: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_domotica_sala_comun_otros)
+
+        var imCafe = findViewById(R.id.imCafe) as ImageView
+        var imFotocopiadora = findViewById(R.id.imFotocopiadora) as ImageView
+        var imMico = findViewById(R.id.imMico) as ImageView
+
+
+
+        imCafe.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            //Toast.makeText(this@DomoticaDespacho, "1", Toast.LENGTH_SHORT).show()
+            val mIntent = Intent(this, DomoticaFinal::class.java)
+            startActivity(mIntent)
+        }
+        imFotocopiadora.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            val mIntent = Intent(this, DomoticaFinal::class.java)
+            startActivity(mIntent)
+        }
+        imMico.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            //Toast.makeText(this@DomoticaDespacho, "3", Toast.LENGTH_SHORT).show()
+            val mIntent = Intent(this, DomoticaFinal::class.java)
+            startActivity(mIntent)
+        }
+    }
+}
