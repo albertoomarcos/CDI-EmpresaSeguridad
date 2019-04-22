fbced03b0b2e96ba676aa4b060c9aa113d7fe7ad
albertoomarcos <albertoo.marcos@gmail.com>
2019-04-22 18:59:05 +0200

    Cambios 2

---
+++ app/src/main/java/com/example/portatil/cdi_empresaseguridad/CBanio.kt
@@ -0,0 +1,13 @@
+package com.example.portatil.cdi_empresaseguridad
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CBanio: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c_banio)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/portatil/cdi_empresaseguridad/CSalaComun.kt
@@ -0,0 +1,15 @@
+package com.example.portatil.cdi_empresaseguridad
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+
+class CSalaComun: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c_sala_comun)
+
+
+    }
+}
